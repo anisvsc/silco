d835b108c518ad546df1593e162b0258100f7589
Anish Gupta <anishhuwu@gmail.com>
2025-07-01 22:40:40 +0530

    added framer motion

---
+++ src/app/components/SilcoCard.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { motion } from 'motion/react';
+import Image from 'next/image';
+import { useRef } from 'react';
+import { Tilt } from '@/components/ui/tilt';
+
+export default function SilcoCard() {
+  const containerRef = useRef(null);
+
+  return (
+    <div
+      className="flex min-h-screen items-center justify-center overflow-hidden"
+      ref={containerRef}
+    >
+      <motion.div
+        drag
+        dragConstraints={containerRef}
+        whileDrag={{ scale: 1.1 }}
+        whileTap={{ scale: 1.1 }}
+      >
+        <Tilt isRevese rotationFactor={10}>
+          <div className="flex flex-col items-center justify-center gap-4 rounded-4xl border bg-primary-foreground/50 pb-4">
+            <Image
+              alt="silco"
+              className="pointer-events-none rounded-3xl"
+              height={300}
+              src={'/silco.jpg'}
+              width={300}
+            />
+            <p className="inline-block font-bold font-mono text-4xl">SILCO</p>
+          </div>
+        </Tilt>
+      </motion.div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
-import Image from 'next/image';
-import { Tilt } from '@/components/ui/tilt';
+import SilcoCard from './components/SilcoCard';
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center border">
-      <Tilt isRevese rotationFactor={8}>
-        <div className="flex flex-col items-center justify-center gap-4 rounded-4xl border bg-primary-foreground/50 pb-4">
-          <Image
-            alt="silco"
-            className="rounded-3xl"
-            height={300}
-            src={'/silco.jpg'}
-            width={300}
-          />
-          <p className="inline-block font-bold font-mono text-4xl">SILCO</p>
-        </div>
-      </Tilt>
-    </div>
-  );
+  return <SilcoCard />;
 }
